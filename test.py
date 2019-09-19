58893be25a87efee557b094e079e19782cb8c900
zhangbo2008 <15122306087@163.com>
2019-09-19 18:16:23 +0800

    可以用test.py运行成功了

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ from synthesizer import Synthesizer
 from wsgiref import simple_server
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--checkpoint', required=False, default='/tmp/tacotron-20170720/model.ckpt')
+parser.add_argument('--checkpoint', required=False, default='/tmp/tacotron-20180906/model.ckpt')
 parser.add_argument('--port', type=int, default=9000)
 parser.add_argument('--hparams', default='',
                     help='Hyperparameter overrides as a comma-separated list of name=value pairs')
@@ -18,6 +18,6 @@ hparams.parse(args.hparams)
 print(hparams_debug_string())
 synthesizer = Synthesizer()
 
-synthesizer.load(args.checkpoint)
+synthesizer.load('/tmp/tacotron-20180906/model.ckpt')
 res=synthesizer.synthesize('apple i eat')
 print(res)
